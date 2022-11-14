fd0259b9931df2ac5069e17c223ec955ebbe449c
Tuan <tuananhnguyen121020@gmail.com>
2022-11-14 22:52:44 +0700

    Tai Len con me no 1 thu muc

---
+++ CSharpProject/CSharp/CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CSharp;
+namespace std
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 a = new Class1(20);
+            a.show();
+        }
+
+    }
+
+
+}
